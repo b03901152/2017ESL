2361178f7f10c0c2296ad1755d1042e9aa15397f
ChenJingyou <b03901152@ntu.edu.tw>
2017-04-25 11:46:30 +0800

    ex2

--- lab2/GPIO/index.js
+++ lab2/GPIO/index.js
@@ -1,11 +1,11 @@
 var tessel = require('tessel'); // Import tessel
 var pinA0 = tessel.port.B.pin[0]; // Select pin 2 on port A
 var time = 500;
-setInterval(()=>pinA0.read( (error, value) => console.log('A0',value) ),time);
+setInterval(()=>pinA2.read( (error, value) => console.log('A0',value) ),time);
 
 var pinA1 = tessel.port.B.pin[1]; // Select pin 2 on port A
 setInterval(
-	()=>pinA1.analogRead( (error, value) => console.log('A1',value) )
+	()=>pinA2.analogRead( (error, value) => console.log('A1',value) )
 ,time);
 var pinA2 = tessel.port.B.pin[2]; // Select pin 2 on port A
 setInterval(()=>pinA2.analogRead( (error, value) => console.log('A2',value) ),time);
