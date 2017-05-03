8dbd88b8d70e66b076165f4caef7f6f7c2410b97
ChenJingyou <b03901152@ntu.edu.tw>
2017-05-03 19:54:17 +0800

    4 servo

--- lab2/EX2/index.js
+++ lab2/EX2/index.js
@@ -15,32 +15,48 @@ var servo = servolib.use(tessel.port['A']);
 
 var servo1 = 1; // We have a servo plugged in at position 1
 var servo2 = 2; // We have a servo plugged in at position 1
+var servo3 = 3; // We have a servo plugged in at position 1
+var servo4 = 4; // We have a servo plugged in at position 1
 var position1 = 1;
 var position2 = 1;
+var position3 = 0.5;
+var position4 = 0.5;
+setInterval( () => {
+  console.log( position1, position2, position3, position4 );
+}, 500 );
 servo.on('ready', function () {
-  servo.configure(servo1, 0.05, 1, function () {
+  servo.configure(servo1, 0.05, 0.12, function () {
     setInterval( () => {
       servo.move(servo1, position1);
-      // servo.move(servo2, position2);
-      if (position1 > 1)
-        position1 = 0;
-      console.log( "position1: ", position1 );
-      // if (position2 > 1)
-        // position2 = 0;
-    }, 500); // Every 500 milliseconds
-  });
-// servo.configure(servo2, 0.05, 1, function () {
-//     setInterval( () => {
-//       servo.move(servo1, position1);
-//       // servo.move(servo2, position2);
-//       // if (position1 > 1)
-//         // position1 = 0;
-//       console.log( "position2: ", position2 );
-//       if (position2 > 1)
-//         position2 = 0;
-//     }, 500); // Every 500 milliseconds
-//   });
+      servo.move(servo2, position2);
+      servo.move(servo3, 1-position3);
+      servo.move(servo4, position4);
 
+      if (position1 > 1)
+        position1 = 1;
+      else if( position1 < 0 )
+        position1 = 0;
+
+      if (position2 > 1)
+        position2 = 1;
+      else if( position2 < 0 )
+        position2 = 0;
+
+    }, 1); // Every 500 milliseconds
+  });
+  // servo.configure(servo2, 0.05, 1, function () {
+  //   setInterval( () => {
+  //     // servo.move(servo1, position1);
+  //     servo.move(servo2, position2);
+
+  //     // if (position1 > 1)
+  //       // position1 = 0;
+  //     if (position2 > 1 || position2 < 0 )
+  //       position2 = 0.5;
+  //     console.log("position2:" , position2 );
+
+  //   }, 500); // Every 500 milliseconds
+  // });
 
 });
 
@@ -60,17 +76,33 @@ require('tty').setRawMode(true);
 stdin.on('keypress', function (chunk, key) {
   switch( chunk ) {
         case 'w': // up rotate
-            position2 += 0.1;
+            position2 -= 0.05;
             break;
         case 's': // down rotate
-            position2 -= 0.1;
+            position2 += 0.05;
             break;
         case 'a': // left rotate
-            position1 -= 0.1;
+            position1 += 0.05;
             break;
         case 'd': // right
-            position1 -= 0.1;
+            position1 -= 0.05;
             break;
+        case 'i': // right
+            position3 = 1;
+            position4 = 1;
+            break;
+        case 'k': // right
+            position3 = 0;
+            position4 = 0;
+            break;
+        case 'j': // right
+            position4 -= 0.05;
+            break;
+        case 'l': // right
+            position4 += 0.05;
+            break;
+        
+
   }
   process.stdout.write('Get Chunk: ' + chunk + '\n');
 });
