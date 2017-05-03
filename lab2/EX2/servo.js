d70146650ac4a9fcb0f750f95ce93c323ea0ae79
ChenJingyou <b03901152@ntu.edu.tw>
2017-05-03 21:39:35 +0800

    servo

--- lab2/EX2/servo.js
+++ lab2/EX2/servo.js
@@ -112,3 +112,45 @@ stdin.on('keypress', function (chunk, key) {
   }
   process.stdout.write('Get Chunk: ' + chunk + '\n');
 });
+
+
+const av = require("tessel-av");
+const ip = require("ip");
+const os = require("os");
+const http = require("http");
+const port = 8888;
+
+const camera = new av.Camera({
+  fps: 3000,
+  // width: 1280,
+  // height: 720,
+  width: 800,
+  height: 600,
+});
+
+const server = http.createServer((request, response) => {
+
+  if (/frame/.test(request.url)) {
+    console.log("asking for image...");
+
+    response.writeHead(200, { "Content-Type": "image/jpeg" });
+    camera.capture().pipe(response);
+  } else {
+    console.log("asking for web page...");
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.end(`
+      <!doctype html>
+      <html>
+        <head>
+          <title>${os.hostname()}</title>
+        </head>
+        <body>
+          <img src="/frame">
+        </body>
+      </html>
+    `);
+  }
+}).listen(port, () => {
+  console.log(`http://${ip.address()}:${port}`);
+  console.log(`<img src="${camera.url}">`);
+});
