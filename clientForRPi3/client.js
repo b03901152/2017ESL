bd39e77df40396ba399757f7d40080aa4c6e26b5
ChenJingyou <b03901152@ntu.edu.tw>
2017-06-16 02:26:18 +0800

    py

--- clientForRPi3/client.js
+++ clientForRPi3/client.js
@@ -4,23 +4,26 @@ var spawn = require( 'child_process' );
 
 spawn.exec('python3 test.py', 
 	function(err, stdout, stderr){
+	var trade;
+	var productName;
+	if ( stdout == 1 )
+		productName = 'coke';
+	else if ( stdout == 2 )	
+		productName = '7up';
+	else if ( stdout == 3 )
+		productName = 'apple juice';
+	trade = {
+	        productName,
+	        tradeTime: String(new Date()),
+	};
+	fetch( 'http://52.179.13.42:3000/setTrade', {
+	headers: {
+	  Accept: 'application/json',
+	  'Content-Type': 'application/json',
+	},
+	method: 'POST',
+	body: JSON.stringify( trade ),
+	credentials: 'same-origin',
+	} );
 	console.log('stdout: ' + stdout);
-	console.log('stderr: ' + stderr);
 });
-
-var trade = {
-        productName: 'coke',
-        tradeTime: 'Thu Jun 15 2017 20:50:15 GMT+0800 (CST)',
-};
-
-fetch( 'http://52.179.13.42:3000/setTrade', {
-headers: {
-  Accept: 'application/json',
-  'Content-Type': 'application/json',
-},
-method: 'POST',
-body: JSON.stringify( trade ),
-credentials: 'same-origin',
-} )
-
-
--- clientForRPi3/test.py
+++ clientForRPi3/test.py
@@ -1,1 +1,2 @@
-print("test lalalalala");
+print("1");
+#  print 1, 2 ,3 to present buy 3 diff product.
